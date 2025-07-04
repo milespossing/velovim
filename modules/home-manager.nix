f11e30859309d45fedf02fb09f4544d8ab827dd2
Miles Possing <7330275+milespossing@users.noreply.github.com>
2025-07-04 14:17:03 -0500

    Add NixOS and home-manager modules

---
+++ modules/home-manager.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }:
+let
+  flake = builtins.getFlake (toString ../.);
+in
+{
+  imports = [
+    flake.inputs.nixvim.homeManagerModules.nixvim
+    ../config
+  ];
+
+  programs.nixvim.enable = lib.mkDefault true;
+}
