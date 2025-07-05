5125e5a9c9fd2987477ade224a835d864e15b91a
Miles Possing <mp-complete@pm.me>
2025-07-04 19:45:08 -0500

    Remove recursion

--- modules/homeManager.nix
+++ modules/homeManager.nix
@@ -5,9 +5,12 @@
   ...
 }:
 let
-  cfg = config.programs.nixvim;
+  cfg = config.velovim;
 in
 {
   imports = [ nixvim.homeModules.default ];
+  options.velovim = {
+    enable = lib.mkEnableOption "Enable velovim";
+  };
   config.programs.nixvim = lib.mkIf cfg.enable (import ../config);
 }
